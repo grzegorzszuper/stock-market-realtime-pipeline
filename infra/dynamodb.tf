41237d06c543dc9cf2ca00d56f450440c229bf09
Grzesiek <grzegorzszuper2@gmail.com>
2025-08-13 15:49:51 +0200

    Update dynamodb.tf

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,11 +1,18 @@
 resource "aws_dynamodb_table" "clean" {
   name         = "StockCleanedData"
-  billing_mode = "PAY_PER_REQUEST"   # najtaniej na start
+  billing_mode = "PAY_PER_REQUEST"
   hash_key     = "symbol"
   range_key    = "timestamp"
 
-  attribute { name = "symbol";    type = "S" }
-  attribute { name = "timestamp"; type = "S" }
+  attribute {
+    name = "symbol"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
 
   tags = {
     Project = "stock-pipeline"
