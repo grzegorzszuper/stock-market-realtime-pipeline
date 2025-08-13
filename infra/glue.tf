e40547a6d1aebf106ed6357d36b9c2c3356c5f7a
Grzesiek <grzegorzszuper2@gmail.com>
2025-08-13 17:27:04 +0200

    Update glue.tf

--- infra/glue.tf
+++ infra/glue.tf
@@ -1,41 +1,38 @@
-resource "aws_glue_catalog_database" "raw_db" {
-  name = "stock_raw_db"
-}
+########################################
+# Glue Database + Crawler + Log Group  #
+########################################
 
-resource "aws_glue_crawler" "raw_crawler" {
-  name          = "stock-raw-crawler"
-  role          = aws_iam_role.glue_role.arn
-  database_name = aws_glue_catalog_database.raw_db.name
+# Log group dla Glue crawlerów (krótka retencja, oszczędnie)
+resource "aws_cloudwatch_log_group" "glue_crawlers" {
+  name              = "/aws-glue/crawlers"
+  retention_in_days = 3
 
-  s3_target {
-    path = "s3://${aws_s3_bucket.raw_data.bucket}/raw/"
-  }
-
-  # żadnych schedule – uruchomimy ręcznie (taniej)
   tags = {
     Project = "stock-pipeline"
-    Purpose = "glue-crawler"
+    Purpose = "glue-logs"
     Env     = "dev"
   }
 }
 
-# Log group dla Glue crawlerów, krótsza retencja
-resource "aws_cloudwatch_log_group" "glue_crawlers" {
-  name              = "/aws-glue/crawlers"
-  retention_in_days = 3
-  tags = { Project = "stock-pipeline", Purpose = "glue-logs", Env = "dev" }
-}
-
+# Baza w Glue Data Catalog
 resource "aws_glue_catalog_database" "raw_db" {
   name = "stock_raw_db"
 }
 
+# Crawler Glue do danych RAW w S3
 resource "aws_glue_crawler" "raw_crawler" {
   name          = "stock-raw-crawler"
-  role          = aws_iam_role.glue_role.arn
+  role          = aws_iam_role.glue_role.arn           # rola z iam_glue.tf
   database_name = aws_glue_catalog_database.raw_db.name
 
   s3_target {
     path = "s3://${aws_s3_bucket.raw_data.bucket}/raw/"
+  }
+
+  # Bez harmonogramu – uruchamiany ręcznie (taniej)
+  tags = {
+    Project = "stock-pipeline"
+    Purpose = "glue-crawler"
+    Env     = "dev"
   }
 }
